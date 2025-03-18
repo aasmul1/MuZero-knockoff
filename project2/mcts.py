0f0b944f3a56dca511409efc643580758cc0f3ef
Åsmund Løvoll <aasmul@stud.ntnu.no>
2025-03-18 22:51:41 +0100

    Started doing the mcts more modular

--- project2/mcts.py
+++ project2/mcts.py
@@ -4,15 +4,60 @@ import numpy as np
 import random
 
 class Node():
-    def __init__(self, parent, game, action, observation, done):
+    def __init__(self, parent, sim_game, action):
         self.parent = parent
-        self.game = game
+        self.sim_game = sim_game
         self.action = action
-        self.observation = observation
-        self.done = done
         self.children = []
         self.visits = 0
         self.reward = 0
+        
+        
+    def copy(self):
+        return copy.deepcopy(self)
+    
+    def follow_tree_policy(self):
+        if self.visits == 0:
+            return self
+        
+        best_child = self.get_best_child()
+        while True:
+            if (best_child.visits == 0 or best_child.sim_game.is_terminal_state() or len(best_child.children) == 0):
+                break
+            best_child = best_child.get_best_child()
+            
+        return best_child
+        
+    def get_best_child(self):
+        if not self.children:
+            raise ValueError("No children found")
+        
+        children_scores = [ (child, child.getUBCScore()) for child in self.children if child.visits > 0]
+        
+        if not children_scores:
+            return random.choice(self.children)
+        
+        max_value = max(children_scores, key=lambda x: x[1])[1]
+        
+        best_children = [ child for child, score in children_scores if score==max_value ]
+        return random.choice(best_children) if best_children else random.choice(self.children)
+    
+    def expand(self):
+        if self.sim_game.is_terminal_state():
+            return self
+        
+        #enten metode fra game_state_manager eller catch.py
+        actions = self.sim_game.get_legal_actions()
+        if len(self.actions) == 0:
+            print("No actions found")
+            return self
+        
+        children = [ Node(self, self.sim_game.copy().apply(action), action)
+                    for action in actions if action is not None]
+        
+        random.shuffle(children)
+        self.children = children
+        return self.get_best_child()
         
     def getUCBScore(self):
         
@@ -27,31 +72,32 @@ class Node():
             
         return (self.reward / self.visits) + c * np.sqrt(np.log(top_node.visits) / self.visits) 
         
-    def create_child(self):
-        if self.done:
-            return None
-        
-        actions = []
-        games = []
-        
-        for i in range(self.game.actions()):
-            actions.append(i)
-            game.append(copy.deepcopy(self.game))
+    def backpropagate(self, reward):
+        self.visits += 1
+        self.value += (reward - self.value)/self.visits
+        if self.parent:
+            self.parent.backpropagate(reward)
             
-        child = {}
-        
-        '''For each pair, it performs a step in the 
-        game using the action, then creates a new Node 
-        that captures the resulting game state (observation), 
-        reward, and done flag, and stores this Node as a child 
-        associated with that action'''
-        
-        for action, game in zip(actions, games):
-            observation, reward, done, _ = game.step(action)
-            child[action] = Node(game, done, self, observation, action)                        
             
-        self.child = child
+            
+    def get_distribution(self):
+        children_action_indices = []
+        for child in self.children:
+            children_action_indices.append(child.action.get_action_index())
+            
+        distribution = np.zeros(len(self.sim_game.get_actions()))
+        distribution[children_action_indices] = [ child.visits for child in self.children]
+        distribution /= distribution.sum()
+        return distribution
         
+        
+class MCTS:
+    def __init__(self, sim_game, current_node):
+        self.sim_game = sim_game.copy()
+        if current_node:
+            self.current_node = current_node
+        else:
+            Node(None, self.sim_game, None)
     def explore(self):
         
         current_node = self
