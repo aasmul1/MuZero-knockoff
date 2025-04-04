3c8effef729d0615b08c9459cf1cfc36a6814a94
Edward Storlien <edward.storlien@gmail.com>
2025-04-04 19:37:20 +0200

    Update playgrounds

--- project2/playground_catch_game.py
+++ project2/playground_catch_game.py
@@ -1,9 +1,11 @@
 from project2.games.catch_game_state_manager import CatchGameStateManager
+from project2.models.perfect_model import PerfectModel
 
-gs_manager = CatchGameStateManager()
-gs_initial_state = gs_manager.generate_initial_state()
-gs_available_actions = gs_manager.get_legal_actions(gs_initial_state)
-available_actions = gs_manager.get_legal_actions(gs_initial_state)
+gsm = CatchGameStateManager()
+gs_initial_state = gsm.generate_initial_state()
+gs_available_actions = gsm.get_legal_actions(gs_initial_state)
+available_actions = gsm.get_legal_actions(gs_initial_state)
+model = PerfectModel(gsm)
 
 state = gs_initial_state
 
@@ -11,12 +13,12 @@ done = False
 
 while not done:
     print(state)
-    temp_game = gs_manager._create_temp_game(state)
-
+    print(model.predict(model.represent_state(state)))
+    temp_game = gsm._create_temp_game(state)
     print(
         f"Fruit height: {temp_game.height - temp_game.fruit_pos[0] - 1}, distance {abs(temp_game.paddle_pos + temp_game.paddle_size // 2 - temp_game.fruit_pos[1])}")
     action = int(input(f"Which action would you like to take? {available_actions} "))
-    state, reward, done = gs_manager.get_next_state_and_reward(state, action)
-    available_actions = gs_manager.get_legal_actions(state)
+    state, reward, done = gsm.get_next_state_and_reward(state, action)
+    available_actions = gsm.get_legal_actions(state)
     print(f"New state. Reward {reward} from action {action}")
     print()
--- project2/playground_mcts_perfect_model.py
+++ project2/playground_mcts_perfect_model.py
@@ -22,7 +22,7 @@ gs_initial_state = gs_manager.generate_initial_state()
 gs_available_actions = gs_manager.get_legal_actions(gs_initial_state)
 
 model = PerfectModel(gs_manager)
-mcts = MCTS(model, gs_available_actions, simulations=100, discount=1, steps=100)
+mcts = MCTS(model, gs_available_actions, simulations=10, discount=1, steps=100, c_1=2, c_2=20000)
 
 initial_state = model.represent_state(gs_initial_state)
 available_actions = gs_manager.get_legal_actions(initial_state)
@@ -43,7 +43,7 @@ while not done:
     total_reward += reward
 
     runs += 1
-    if runs > 1000:
+    if runs > 100:
         done = True
 
 print(f"Total reward: {total_reward}")
