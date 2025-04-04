ae02ce00007166f86f3e839081bff714d8bceb32
Edward Storlien <edward.storlien@gmail.com>
2025-04-04 21:58:37 +0200

    Refactor

--- project2/models/init.py
+++
@@ -1,5 +0,0 @@
-from models.super_model import Model
-from models.perfect_model import PerfectModel
-from models.muzero_model import MuZeroModel
-
-__all__ = ['Model', 'PerfectModel', 'MuZeroModel']
